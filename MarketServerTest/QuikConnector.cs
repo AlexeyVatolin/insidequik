f89d6834ef4439e37dcd7d4e3273df03fd6f9df3
AlexeyVatolin <vatolinalex@gmail.com>
2017-11-22 21:18:51 +0500

    Создан каркас проекта, добавлены необходимые зависимости

---
+++ MarketServerTest/QuikConnector.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuikSharp;
+using QuikSharp.DataStructures;
+
+namespace MarketServerTest
+{
+    static class QuikConnector
+    {
+        private static Quik Quik;
+        public static bool isConnected { get; private set; }
+
+        public static bool Connect()
+        {
+            Quik = new Quik(Quik.DefaultPort, new InMemoryStorage());
+            if (Quik != null && Quik.Service.IsConnected().Result)
+            {
+                isConnected = true;
+                return true;
+            }
+            isConnected = false;
+            return false;
+        }
+
+        public static OrderBook SubscribeToOrderBook()
+        {
+            return new OrderBook();
+        }
+
+
+    }
+}

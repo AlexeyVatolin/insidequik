f7e2e8ef9027dbb3ab236d4e94a7094d4ea10703
AlexeyVatolin <vatolinalex@gmail.com>
2017-11-24 21:46:35 +0500

    Добавлена загрука стакана

--- MarketServerTest/QuikConnector.cs
+++ MarketServerTest/QuikConnector.cs
@@ -12,6 +12,7 @@ namespace MarketServerTest
     {
         private static Quik Quik;
         public static bool isConnected { get; private set; }
+        public delegate void OnQuoteDoDelegate(OrderBook quote);
 
         public static bool Connect()
         {
@@ -25,11 +26,33 @@ namespace MarketServerTest
             return false;
         }
 
-        public static OrderBook SubscribeToOrderBook()
+        public static void SubscribeToOrderBook(string ticker, QuoteHandler quoteDoDelegate)
         {
-            return new OrderBook();
+            //TODO: разобраться что значит эта мифическая строка SPBFUT,TQBR..
+            string classCode = Quik.Class.GetSecurityClass("SPBFUT,TQBR,TQBS,TQNL,TQLV,TQNE,TQOB", ticker).Result;
+            if (!string.IsNullOrEmpty(classCode))
+            {
+                var tool = new Tool(Quik, ticker, classCode);
+                if (!string.IsNullOrEmpty(tool.Name))
+                {
+                    Quik.OrderBook.Subscribe(tool.ClassCode, tool.SecurityCode).Wait();
+                    bool isSubscribedToolOrderBook = Quik.OrderBook.IsSubscribed(tool.ClassCode, tool.SecurityCode).Result;
+                    if (isSubscribedToolOrderBook)
+                    {
+                        Console.WriteLine("Успешно подписался на " + ticker); //TODO: убрать когда будет проверено
+                        Quik.Events.OnQuote += quoteDoDelegate;
+                    }
+                    else
+                    {
+                        throw new Exception();
+                    }
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+
         }
-
-
     }
 }

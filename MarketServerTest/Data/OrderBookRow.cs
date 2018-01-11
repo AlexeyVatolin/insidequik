176c580ef124ee1991a6df127ab67f669e26fb81
AlexeyVatolin <vatolinalex@gmail.com>
2018-01-11 20:16:11 +0500

    Сделал красивый стиль для строк ListView в стакане, исправил ошибку с исчезновением выделения строки при обновлении стакана

---
+++ MarketServerTest/Data/OrderBookRow.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuikSharp.DataStructures;
+
+namespace MarketServerTest.Data
+{
+    class OrderBookRow : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private double _price;
+        private double _quantity;
+        private string _type;
+
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                _price = value;
+                OnPropertyChanged(nameof(Price));
+            }
+        }
+
+        public double Quantity
+        {
+            get => _quantity;
+            set
+            {
+                _quantity = value;
+                OnPropertyChanged(nameof(Quantity));
+            }
+        }
+
+        public string Type
+        {
+            get => _type;
+            set
+            {
+                _type = value;
+                OnPropertyChanged(nameof(Type));
+            }
+        }
+
+        public OrderBookRow(OrderBook.PriceQuantity priceQuantity, string type)
+        {
+            Price = priceQuantity.price;
+            Quantity = priceQuantity.quantity;
+            Type = type;
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
